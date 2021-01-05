ba72e78e27b2fe07f00ff4c1e5636ad035802c24
ColdCoffeeDev-Kia <77003943+ColdCoffeeDev-Kia@users.noreply.github.com>
2021-01-05 17:43:13 +0330

    MainMenu1
    I created the menu Scene and Imported the background, and created a filmGrain script.

---
+++ JerrysDestiny/Assets/Scripts/FilmGrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilmGrain : MonoBehaviour
+{
+    public float ChangeSpeed = 0.0001f;
+
+    public float MinX, MaxX, MinY, MaxY;
+
+    private float lastUpdate = 0f;
+    private void Update()
+    {
+
+        if (Time.time - lastUpdate >= ChangeSpeed)
+        {
+            float x = UnityEngine.Random.Range(MinX, MaxX);
+            float y = UnityEngine.Random.Range(MinY, MaxY);
+
+            GetComponent<RectTransform>().position = new Vector3(x, y, 0f);
+            
+            lastUpdate = Time.time;
+        }
+    }
+}
