f8888e790d01e9c411030b6d168747303ff641f4
ColdCoffeeDev-Kia <77003943+ColdCoffeeDev-Kia@users.noreply.github.com>
2021-01-07 18:10:18 +0330

    a little edit and 3d folder

---
+++ JerrysDestiny/Assets/Scripts/Menu/GraphicsButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsButton : MonoBehaviour
+{
+    [SerializeField] public Button Fast;
+    [SerializeField] public Button Fancy;
+
+    public Color EnabledCC;
+    public Color DisabledCC;
+
+    public void EnableFast()
+    {
+            
+    }
+
+    public void EnableFancy()
+    {
+        
+    }
+}
